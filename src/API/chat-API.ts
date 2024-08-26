45d8cb611fda322c09f460f02078a32eb2a3649d
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-08-27 01:10:24 +0300

    fix

--- src/API/chat-API.ts
+++ src/API/chat-API.ts
@@ -7,12 +7,12 @@ export const CreateChat = async (owner_id: string, data: { firstName: string; la
     return response.data
 }
 
-export const UpdateChat = async (owner_id: string, chat_id: string, data: { firstName: string; lastName: string }) => {
+export const UpdateChat = async (owner_id: string, chat_id: string | undefined, data: { firstName: string; lastName: string }) => {
     const response = await axios.patch(`/chats/update/${chat_id}`, data, { params: { userId: owner_id } })
     return response.data
 }
 
-export const DeleteChat = async (owner_id: string, chat_id: string) => {
+export const DeleteChat = async (owner_id: string, chat_id: string | undefined) => {
     const response = await axios.delete(`/chats/delete/${chat_id}`, { params: { userId: owner_id } })
     return response.data
 }
