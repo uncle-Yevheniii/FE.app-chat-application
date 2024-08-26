ac4755075ce6bb0f16f8277bb65234c9e1a88373
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-08-27 00:15:03 +0300

    add last style

--- src/pages/authorization/authorization.tsx
+++ src/pages/authorization/authorization.tsx
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet-async'
 import { AuthorizationForm } from '@/components/authorization-form/authorization-form'
 import { IUserData } from '@/components/type'
 
+import style from './style.module.css'
+
 interface AuthorizationProps {
     setLocalStorage: Dispatch<SetStateAction<IUserData | null>>
 }
@@ -14,7 +16,9 @@ export default function Authorization({ setLocalStorage }: AuthorizationProps) {
                 <title>Authorization</title>
             </Helmet>
 
-            <AuthorizationForm setLocalStorage={setLocalStorage} />
+            <div className={style.container}>
+                <AuthorizationForm setLocalStorage={setLocalStorage} />
+            </div>
         </div>
     )
 }
