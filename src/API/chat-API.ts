9a61c8f5df6dafc7ae01b596f6cea74cd9dba593
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-08-27 00:54:00 +0300

    fix

--- src/API/chat-API.ts
+++ src/API/chat-API.ts
@@ -1,14 +1,13 @@
-import { IUserData } from '@/components/type'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://be-app-chat-application.onrender.com'
 
-export const CreateChat = async (owner_id: string, data: IUserData) => {
+export const CreateChat = async (owner_id: string, data: { firstName: string; lastName: string }) => {
     const response = await axios.post('/chats/create', data, { params: { userId: owner_id } })
     return response.data
 }
 
-export const UpdateChat = async (owner_id: string, chat_id: string, data: IUserData) => {
+export const UpdateChat = async (owner_id: string, chat_id: string, data: { firstName: string; lastName: string }) => {
     const response = await axios.patch(`/chats/update/${chat_id}`, data, { params: { userId: owner_id } })
     return response.data
 }
