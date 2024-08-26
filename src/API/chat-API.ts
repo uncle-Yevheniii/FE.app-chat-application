9bf35c1029ae3af2c91ad7ee2801590093bd1945
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-08-27 00:50:22 +0300

    typing

--- src/API/chat-API.ts
+++ src/API/chat-API.ts
@@ -1,18 +1,19 @@
+import { IUserData } from '@/components/type'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://be-app-chat-application.onrender.com'
 
-export const CreateChat = async (owner_id, data) => {
+export const CreateChat = async (owner_id: string, data: IUserData) => {
     const response = await axios.post('/chats/create', data, { params: { userId: owner_id } })
     return response.data
 }
 
-export const UpdateChat = async (owner_id, chat_id, data) => {
+export const UpdateChat = async (owner_id: string, chat_id: string, data: IUserData) => {
     const response = await axios.patch(`/chats/update/${chat_id}`, data, { params: { userId: owner_id } })
     return response.data
 }
 
-export const DeleteChat = async (owner_id, chat_id) => {
+export const DeleteChat = async (owner_id: string, chat_id: string) => {
     const response = await axios.delete(`/chats/delete/${chat_id}`, { params: { userId: owner_id } })
     return response.data
 }
